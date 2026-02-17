6de20c18f50ff9070266fd9d87e0fad7b4bcde75
Spongebosch <minecraft.bosch@gmail.com>
2026-02-17 18:02:48 -0500

    Some fixes
    Changed a couple of wrong types, fixed the error handling, and such

--- project1/lib/core_interp.ml
+++ project1/lib/core_interp.ml
@@ -55,7 +55,8 @@ module Env = struct
    *)
 (*! env lookup !*)
   let lookup (rho : t) (x : Ast.Id.t) : Value.t = 
-    List.assoc x rho
+    try List.assoc x rho with
+    | Not_found -> raise (UnboundVariable x)
 (*! end !*)
 
   (*  update ρ x v = ρ{x → v}.
@@ -72,7 +73,7 @@ let unop (op: E.unop) (v : Value.t) : Value.t =
   match (op, v) with
   | (E.Neg, Value.V_Int n) -> Value.V_Int (-n)
   | (E.Not, Value.V_Bool b) -> Value.V_Bool (not b)
-  | _ -> failwith "Unimplemented"
+  | _ -> raise (TypeError "Should call you Alexandre, cause you're a Dumas")
 
 
 (*  binop op v0 v1 = v', where v' is the result of applying the semantic
@@ -83,7 +84,7 @@ let binop (op : E.binop) (v0 : Value.t) (v1 : Value.t) : Value.t =
   (*Basic arithmetic operations*)
   | (E.Plus, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 + n1)
   | (E.Minus, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 - n1)
-  | (E.Times, Value.V_Int n0, Value.V_Int n1) -> Vale.V_Int (n0 * n1)
+  | (E.Times, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 * n1)
   | (E.Div, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 / n1)
   | (E.Mod, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 mod n1)
   (*Basic boolean operations*)
@@ -95,36 +96,38 @@ let binop (op : E.binop) (v0 : Value.t) (v1 : Value.t) : Value.t =
   | (E.Ne, Value.V_Int n0, Value.V_Int n1) -> Value.V_Bool (n0 != n1)
   | (E.Ne, Value.V_Bool b0, Value.V_Bool b1) -> Value.V_Bool (b0 != b1)
   (*Ineqality operations*)
-  | (E.Lt, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 < n1)
-  | (E.Le, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 <= n1)
-  | (E.Gt, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 > n1)
-  | (E.Ge, Value.V_Int n0, Value.V_Int n1) -> Value.V_Int (n0 >= n1)
-  | _ -> failwith "Unimplemented"
+  | (E.Lt, Value.V_Int n0, Value.V_Int n1) -> Value.V_Bool (n0 < n1)
+  | (E.Le, Value.V_Int n0, Value.V_Int n1) -> Value.V_Bool (n0 <= n1)
+  | (E.Gt, Value.V_Int n0, Value.V_Int n1) -> Value.V_Bool (n0 > n1)
+  | (E.Ge, Value.V_Int n0, Value.V_Int n1) -> Value.V_Bool (n0 >= n1)
+  | _ -> raise (TypeError "Should call you Alexandre, cause you're a Dumas")
 
 
 (*  eval ρ e = v, where ρ ├ e ↓ v according to our evaluation rules.
  *)
 let rec eval (rho : Env.t) (funs : Ast.Script.fundef list) (e : E.t) : Value.t =
   match e with
-  | E.Var s -> Env.lookup rho s
+  | E.Var x -> Env.lookup rho x
   | E.Num n -> Value.V_Int n
-  | E.Bool b -> b
+  | E.Bool b -> Value.V_Bool b
   | E.Unop (op, e') ->
-    let v = eval rho e' in unop op v
+    let v = eval rho funs e' in unop op v
   | E.Binop (op, e0, e1) -> 
-    let v0 = eval rho e0 in
-    let v1 = eval rho e1 in
+    let v0 = eval rho funs e0 in
+    let v1 = eval rho funs e1 in
     binop op v0 v1
   | E.If(e0, e1, e2) ->
-    if eval rho e0 then eval rho e1 else eval rho e2
+    (match eval rho funs e0 with
+    | (Value.V_Bool b) -> if b then eval rho funs e1 else eval rho funs e2
+    | _ -> raise (TypeError "Should call you Alexandre, cause you're a Dumas"))
   | E.Let(x, e0, e1) ->
-    let x' = eval rho e0 in
-    let rho' = Env.addrho rho x m in eval rho' e1
-  | E.Call(f, es) -> failwith "Unimplemented"
+    let x' = eval rho funs e0 in
+    let rho' = Env.addrho rho x x' in eval rho' funs e1
+  | _ -> failwith "Unimplemented"
 
                           
 (* exec p = v, where `v` is the result of executing `p`.
  *)
 let exec (p : Ast.Script.t) : Value.t =
   match p with
-  | (funs, e) -> eval Env.empty funs e
+  | Ast.Script.Pgm (funs, e) -> eval Env.empty funs e
